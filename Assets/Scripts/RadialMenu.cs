b16967c1748bb22dbd1d17078359734a2992214c
Judah Rosaria <judahboy@live.com>
2021-09-07 17:18:54 +0200

    -implemented shitty menu rotation

--- Assets/Scripts/RadialMenu.cs
+++ Assets/Scripts/RadialMenu.cs
@@ -5,21 +5,28 @@ using UnityEngine.UI;
 
 public class RadialMenu : MonoBehaviour
 {
+    public float speed = 0.5f;
     public Text label;
     public RadialButton buttonPrefab;
     public RadialButton selected;
+    private int selectedInt;
+    private RadialButton[] buttons;
+    private Vector3[] newButtonPositions;
 
     // Start is called before the first frame update
     public void SpawnButtons(Interactable obj)
     {
-         StartCoroutine(AnimateButtons(obj));
+        StartCoroutine(AnimateButtons(obj));
     }
 
     IEnumerator AnimateButtons(Interactable obj)
     {
-        for (int i = 0; i < obj.options.Length; i++)
-        {
+        buttons = new RadialButton[obj.options.Length];
+        newButtonPositions = new Vector3[obj.options.Length];
+
+        for (int i = 0; i < obj.options.Length; i++) {
             RadialButton newButton = Instantiate(buttonPrefab) as RadialButton;
+            buttons[i] = newButton;
             newButton.transform.SetParent(transform, false);
             float theta = (2 * Mathf.PI / obj.options.Length) * i;
             float xPos = Mathf.Sin(theta);
@@ -36,13 +43,52 @@ public class RadialMenu : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            if (selected)
-            {
+        if (Input.GetKeyUp(KeyCode.Space)) {
+            if (selected) {
                 Debug.Log(selected.title + " was selected.");
             }
             Destroy(gameObject);
+        }
+
+        if (Input.GetKeyUp(KeyCode.RightArrow)) {
+            TurnMenu(1);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            TurnMenu(-1);
+        }
+    }
+
+    public void TurnMenu(int tileAmount)
+    {
+        selectedInt += tileAmount;
+        for (int i = 0; i < buttons.Length; i++) {
+            float theta = (2 * Mathf.PI / buttons.Length) * (i + selectedInt);
+            float xPos = Mathf.Sin(theta);
+            float yPos = Mathf.Cos(theta);
+            newButtonPositions[i] = new Vector2(xPos, yPos) * 100f;
+        }
+        StartCoroutine(TurnAnimation());
+    }
+
+    IEnumerator TurnAnimation()
+    {
+        float timer = 0f;
+        Vector3[] oldButtonPos = new Vector3[buttons.Length];
+
+        for(int i=0; i<buttons.Length; i++) {
+            oldButtonPos[i] = buttons[i].transform.localPosition;
+        }
+
+        while (timer < 1 / speed) {
+            timer += Time.deltaTime;
+            for(int i=0; i<buttons.Length; i++) {
+                buttons[i].transform.localPosition = Vector3.Lerp(oldButtonPos[i] , newButtonPositions[i], timer);
+            }
+            yield return null;
+        }
+
+        for (int i = 0; i < buttons.Length; i++) {
+            buttons[i].transform.localPosition = newButtonPositions[i];
         }
     }
 }
