37d38aa46f01c1e05a16981999e07036131b42ed
Judah Rosaria <judahboy@live.com>
2021-09-16 11:43:50 +0200

    ~Helper class methods public

--- Assets/Scripts/Helper.cs
+++ Assets/Scripts/Helper.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 static class Helper
 {
-    static float CalculateCirc(float pRadius)
+    public static float CalculateCircm(float pRadius)
     {
         return (2 * Mathf.PI * pRadius);
     }
 
-    static float CalculateCircDeg(float pRadius)
+    public static float CalculateCircmDeg(float pRadius)
     {
-        return 360 / CalculateCirc(pRadius);
+        return 360 / CalculateCircm(pRadius);
     }
 
-    static Vector2 CalculateDegPos(float deg, float pRadius)
+    public static Vector2 CalculateDegPos(float deg, float pRadius)
     {
         float x = pRadius * Mathf.Cos(deg * Mathf.Deg2Rad);
         float y = x * Mathf.Tan(deg * Mathf.Deg2Rad);
